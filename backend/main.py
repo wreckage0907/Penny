02b129dac8589d55504b7c2b295154966e4ee8bd
wreckage0907 <girish.raghav2004@gmail.com>
2024-06-09 22:54:22 +0530

    Initial Commit

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
