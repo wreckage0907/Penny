6040192980bf8ccb6bc263216e0e4b22ede9db88
wreckage0907 <girish.raghav2004@gmail.com>
2024-06-20 22:01:54 +0530

    ADDED SCROLL VIEW IN LOGIN AND SIGNUP PAGE

--- mobile/lib/Pages/login/signup.dart
+++ mobile/lib/Pages/login/signup.dart
@@ -135,116 +135,121 @@ class _SignUpPageState extends State<SignUpPage> {
       backgroundColor: const Color.fromRGBO(255, 246, 229, 1),
       body: Padding(
         padding: const EdgeInsets.all(28),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Container(
-                width: 200,
-                child: const Image(
-                  image: AssetImage('assets/title.png'),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Center(
-              child: Text(
-                'Welcome!',
-                style: GoogleFonts.spectral(
-                  fontSize: 45,
-                  fontWeight: FontWeight.w300,
-                  color: Colors.black87,
-                ),
-              ),
-            ),
-            const SizedBox(height: 26),
-            UsernameField(controller: _usernameController,),
-            const SizedBox(height: 14),
-            EmailField(controller: _emailController,),
-            const SizedBox(height: 14),
-            PasswordField(controller: _passwordController,),
-            const SizedBox(height: 25),
-            Center(
-              child: Text(
-                'or continue with',
-                style: GoogleFonts.spectral(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w300,
-                  color: Colors.black87,
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SignInWithGoogleButton(),
-                FacebookButton(),
-                AppleButton()
-              ],
-            ),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.only(left: 20,right:20,top: 20,bottom: 5),
-              child: TextButton(
-                onPressed: () async {
-                  _authService.signUp(username:_usernameController.text,email: _emailController.text,password:_passwordController.text);
-                },
-                style: const ButtonStyle(
-                  alignment: Alignment.center,
-                  backgroundColor: WidgetStatePropertyAll(Color.fromRGBO(175, 92, 92, 0.8)),
-                  padding: WidgetStatePropertyAll(EdgeInsets.symmetric(vertical: 16, horizontal: 24)),
-                  minimumSize: WidgetStatePropertyAll(Size(double.infinity, 24)),
-                  shape: WidgetStatePropertyAll(
-                    RoundedRectangleBorder(
-                      side: BorderSide(
-                        color: Color.fromRGBO(109, 109, 109, 1),
-                        width: 1.5,
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(28)),
-                    ),
+
+        child: SingleChildScrollView(
+          child: ConstrainedBox(constraints: const BoxConstraints(minHeight: 700),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Container(
+                  width: 200,
+                  child: const Image(
+                    image: AssetImage('assets/title.png'),
                   ),
                 ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Center(
                 child: Text(
-                  'Sign Up',
+                  'Welcome!',
                   style: GoogleFonts.spectral(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                    fontSize: 45,
+                    fontWeight: FontWeight.w300,
+                    color: Colors.black87,
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 30),
-            Center(
-              child: RichText(
-                text: TextSpan(
+              const SizedBox(height: 26),
+              UsernameField(controller: _usernameController,),
+              const SizedBox(height: 14),
+              EmailField(controller: _emailController,),
+              const SizedBox(height: 14),
+              PasswordField(controller: _passwordController,),
+              const SizedBox(height: 25),
+              Center(
+                child: Text(
+                  'or continue with',
                   style: GoogleFonts.spectral(
-                    fontSize: 19,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w300,
+                    color: Colors.black87,
                   ),
-                  children: [
-                    const TextSpan(text: 'Already have an account? '),
-                    TextSpan(
-                      text: 'Sign In',
-                      style: const TextStyle(
-                        color: Colors.redAccent,
-                        decoration: TextDecoration.underline,
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SignInWithGoogleButton(),
+                  FacebookButton(),
+                  AppleButton()
+                ],
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(left: 20,right:20,top: 20,bottom: 5),
+                child: TextButton(
+                  onPressed: () async {
+                    _authService.signUp(username:_usernameController.text,email: _emailController.text,password:_passwordController.text);
+                  },
+                  style: const ButtonStyle(
+                    alignment: Alignment.center,
+                    backgroundColor: WidgetStatePropertyAll(Color.fromRGBO(175, 92, 92, 0.8)),
+                    padding: WidgetStatePropertyAll(EdgeInsets.symmetric(vertical: 16, horizontal: 24)),
+                    minimumSize: WidgetStatePropertyAll(Size(double.infinity, 24)),
+                    shape: WidgetStatePropertyAll(
+                      RoundedRectangleBorder(
+                        side: BorderSide(
+                          color: Color.fromRGBO(109, 109, 109, 1),
+                          width: 1.5,
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(28)),
                       ),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          Navigator.pushNamed(context, '/login');
-                        },
                     ),
-                  ],
+                  ),
+                  child: Text(
+                    'Sign Up',
+                    style: GoogleFonts.spectral(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(height: 30),
+              Center(
+                child: RichText(
+                  text: TextSpan(
+                    style: GoogleFonts.spectral(
+                      fontSize: 19,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black,
+                    ),
+                    children: [
+                      const TextSpan(text: 'Already have an account? '),
+                      TextSpan(
+                        text: 'Sign In',
+                        style: const TextStyle(
+                          color: Colors.redAccent,
+                          decoration: TextDecoration.underline,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Navigator.pushNamed(context, '/login');
+                          },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
         ),
       ),
     );
