a30241508ac16df54bd6f6dba17fb0ec96a297e1
Chandani122 <pcchandani122@gmail.com>
2024-07-07 15:50:26 +0530

    Rebase

--- backend/database/firebase_init.py
+++ backend/database/firebase_init.py
@@ -3,7 +3,7 @@ import os
 
 def initialize_firebase():
     current = os.path.dirname(__file__)
-    path = os.path.join(current, "../penny-89979-firebase-adminsdk-spc33-5699359ee2.json")
+    path = os.path.join(current, "../firebase.json")
     cred = credentials.Certificate(path)
     firebase_app = initialize_app(cred, {
         'storageBucket': 'penny-89979.appspot.com'
--- backend/routers/chatbot.py
+++ backend/routers/chatbot.py
@@ -12,19 +12,19 @@ def get_chat_history(user_id: str):
     else:
         raise HTTPException(status_code=404, detail="User not found")
     
-@router.post("/chatbot/{user_id}/add/{chat}")
+@router.post("/chatbot/{user_id}/add")
 def add_chat_history(user_id: str, chat: dict):
     chat_ref = db.collection("chatbot").document(user_id)
     chat_ref.set(chat)
     return chat
 
-@router.put("/chatbot/{user_id}/update/{chat}")
+@router.put("/chatbot/{user_id}/update")
 def update_chat_history(user_id: str, chat: dict):
     chat_ref = db.collection("chatbot").document(user_id)
     chat_ref.update(chat)
     return chat
 
-@router.delete("/chatbot/{user_id}/delete/{chat}")
+@router.delete("/chatbot/{user_id}/delete")
 def delete_chat_history(user_id: str):
     chat_ref = db.collection("chatbot").document(user_id)
     chat_ref.delete()
