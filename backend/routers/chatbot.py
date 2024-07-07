a2017437136f8fd96408d05ac170dc233ef3d536
terfefed <alljoe947@gmail.com>
2024-07-07 21:18:14 +0530

    Your descriptive commit message

--- backend/routers/chatbot.py
+++ backend/routers/chatbot.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, HTTPException
-from database.firebase_init import db
+from database.firebase_init import firestore_db
 
 router = APIRouter()
 
 @router.get("/chatbot/{user_id}")
 def get_chat_history(user_id: str):
-    chat_ref = db.collection("chatbot").document(user_id)
+    chat_ref = firestore_db.collection(user_id).document("chatbot")
     chats = chat_ref.get()
     if chats.exists:
         return chats.to_dict()
@@ -14,18 +14,18 @@ def get_chat_history(user_id: str):
     
 @router.post("/chatbot/{user_id}/add")
 def add_chat_history(user_id: str, chat: dict):
-    chat_ref = db.collection("chatbot").document(user_id)
+    chat_ref = firestore_db.collection(user_id).document("chatbot")
     chat_ref.set(chat)
     return chat
 
 @router.put("/chatbot/{user_id}/update")
 def update_chat_history(user_id: str, chat: dict):
-    chat_ref = db.collection("chatbot").document(user_id)
+    chat_ref = firestore_db.collection(user_id).document("chatbot")
     chat_ref.update(chat)
     return chat
 
 @router.delete("/chatbot/{user_id}/delete")
 def delete_chat_history(user_id: str):
-    chat_ref = db.collection("chatbot").document(user_id)
+    chat_ref = firestore_db.collection(user_id).document("chatbot")
     chat_ref.delete()
     return {"message": "Chat history deleted"}
